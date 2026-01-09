36aba6a41965a4684182f943494b5eec17eb4600
vijayagavhane2002 <vijayagavhane2002@gmail.com>
2026-01-09 13:18:20 +0530

    Fix base path for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   // FIX: Change base to '/' for Vercel deployment
   plugins: [react(), tailwindcss()],
-  base: '/VAPLNewWebsite', 
+  base: '/VAPLNewWebsite/', 
 })
