4785c5850374e3f33b571c74c354c99d6774d05d
vijayagavhane2002 <vijayagavhane2002@gmail.com>
2026-01-09 18:10:17 +0530

    Fix entry file case sensitivity

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-// We do NOT add <BrowserRouter> here because it is already in App.tsx
 createRoot(document.getElementById("root") as HTMLElement).render(
     <App />
 );
