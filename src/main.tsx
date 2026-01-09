ab3bf6c41e3bf47c224c25f78ae4ad7c05967247
vijayagavhane2002 <vijayagavhane2002@gmail.com>
2026-01-09 18:02:20 +0530

    final commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import "./index.css";
 
 // We do NOT add <BrowserRouter> here because it is already in App.tsx
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("root") as HTMLElement).render(
     <App />
 );
